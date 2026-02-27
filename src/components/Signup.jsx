06a41d929f65371132e49bc07bfd629d45ae40d2
Julius Aako <aakojuiusoluwanifemi@gmail.com>
2026-02-27 14:27:26 +0100

    blank display fixed on signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -71,90 +71,88 @@ const Signup = () => {
   return (
     <div className="max-w-md bg-white shadow w-full mx-auto flex items-center justify-center my-20">
       <div className="modal-action flex flex-col justify-center mt-0">
-        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="card-body"
-            method="dialog"
-          >
-            <h3 className="font-bold text-lg text-center">Register Here!</h3>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="name"
-                className="input input-bordered"
-                {...register("name")}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-                {...register("email")}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-                {...register("password")}
-              />
-            </div>
-            {/* Error test */}
-
-            {/* login button */}
-            <div className="form-control mt-6">
-              <input
-                type="submit"
-                value="Signup"
-                className="btn bg-success text-white"
-              />
-            </div>
-            <p className="text-center my-2">
-              Already have an account?{" "}
-              <button
-                onClick={() => document.getElementById("my_modal_5").showModal()}
-                className="underline text-red-700 ml-1"
-              >
-                Login
-              </button>{" "}
-            </p>
-            <Link
-              to="/"
-              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 my-10"
-            >
-              ✕
-            </Link>
-          </form>
-          {/* Social Login */}
-          <div className="text-center space-x-3 my-3">
-            <button
-              className="btn btn-circle hover:bg-success hover:text-white"
-              onClick={handleSignup}
-            >
-              <FaGoogle />
-            </button>
-            <button className="btn btn-circle hover:bg-blue-700 hover:text-white">
-              <FaFacebookF />
-            </button>
-            <button className="btn btn-circle hover:bg-black hover:text-white">
-              <FaGithub />
-            </button>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="card-body"
+          method="dialog"
+        >
+          <h3 className="font-bold text-lg text-center">Register Here!</h3>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="name"
+              className="input input-bordered"
+              {...register("name")}
+            />
           </div>
-        </dialog>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+              {...register("email")}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+              {...register("password")}
+            />
+          </div>
+          {/* Error test */}
+
+          {/* login button */}
+          <div className="form-control mt-6">
+            <input
+              type="submit"
+              value="Signup"
+              className="btn bg-success text-white"
+            />
+          </div>
+          <p className="text-center my-2">
+            Already have an account?{" "}
+            <button
+              onClick={() => document.getElementById("my_modal_5").showModal()}
+              className="underline text-red-700 ml-1"
+            >
+              Login
+            </button>{" "}
+          </p>
+          <Link
+            to="/"
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 my-10"
+          >
+            ✕
+          </Link>
+        </form>
+        {/* Social Login */}
+        <div className="text-center space-x-3 my-3">
+          <button
+            className="btn btn-circle hover:bg-success hover:text-white"
+            onClick={handleSignup}
+          >
+            <FaGoogle />
+          </button>
+          <button className="btn btn-circle hover:bg-blue-700 hover:text-white">
+            <FaFacebookF />
+          </button>
+          <button className="btn btn-circle hover:bg-black hover:text-white">
+            <FaGithub />
+          </button>
+        </div>
       </div>
       <Modal />
     </div>
